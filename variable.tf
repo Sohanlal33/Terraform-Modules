0f2c9818fa83ec181f017ca41199957f3142d547
Sohanlal Chaudhary <163651175+Sohanlal33@users.noreply.github.com>
2024-04-27 19:42:44 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,153 @@
+variable "aws_region" {
+  description = "Aws region"
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "ID of Ami to Provision"
+  default     = "ami-04b70f"
+}
+
+variable "instance_type" {
+  description = "Type of instance to be provisioned"
+  default     = "t2.micro"
+}
+
+variable "name" {
+  description = "Name to pass to Name tags "
+  default     = "Provisioned"
+}
+variable "bucket_name" {
+  description = "Name to pass to Name tags "
+  default     = "new-bucket-3"
+
+}
+
+variable "database_route_table_association_required" {
+  description = "Whether db route table association required"
+  type        = bool
+  default     = null
+}
+
+variable "create_igw" {
+  description = "Whether IGW needs to be created"
+  type        = bool
+  default     = null
+}
+variable "igw_tag" {
+  description = "Mention Tag needs to be associated with internet gateway"
+  type        = string
+  default     = "new_igw"
+}
+variable "public_route_table_tag" {
+  description = "Tag name for public route table"
+  type        = string
+  default     = "new_public_route_table"
+}
+variable "database_route_table_tag" {
+  description = "Tag for database route table"
+  type        = string
+  default     = "new_database_route_table"
+}
+variable "cidr" {
+  description = "Enter the CIDR range required for VPC"
+  type        = string
+  default     = "10.0.0.0/24"
+}
+variable "enable_dns_hostnames" {
+  description = "Enable DNS Hostname"
+  type        = bool
+  default     = null
+}
+
+variable "enable_dns_support" {
+  description = "Enable DNS Support"
+  type        = bool
+  default     = null
+}
+variable "enable_ipv6" {
+  description = "Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC. You cannot specify the range of IP addresses, or the size of the CIDR block."
+  type        = bool
+  default     = null
+}
+
+variable "vpc_name" {
+  description = "Tag Name to be assigned with VPC"
+  type        = string
+  default     = "new_vpc"
+}
+
+variable "default_security_group_name" {
+  description = "Enter the name for security group"
+  type        = string
+  default     = null
+}
+
+variable "enable_dhcp_options" {
+  description = "Enable DHCP options.. True or False"
+  type        = bool
+  default     = null
+}
+
+variable "manage_default_route_table" {
+  description = "Are we managing default route table"
+  type        = bool
+  default     = null
+}
+variable "public_subnet_tag_1" {
+  description = "Tag for public subnet"
+  type        = string
+  default     = "tcw_public_subnet_az_1a"
+}
+variable "public_subnet_tag_2" {
+  description = "Tag for public subnet"
+  type        = string
+  default     = "tcw_public_subnet_az_1b"
+}
+variable "database_subnets" {
+  description = "CIDR block for database subnet"
+  type        = list(any)
+  default     = null
+}
+
+variable "public_subnet" {
+  description = "enter the number of public subnets you need"
+  type        = number
+  default     = null
+}
+
+variable "public_subnets_cidr_1" {
+  description = "Cidr Blocks"
+  type        = string
+  default     = "10.1.0.0/24"
+}
+variable "public_subnets_cidr_2" {
+  description = "Cidr Blocks"
+  type        = string
+  default     = "10.2.0.0/24"
+}
+variable "database_subnet_tag_1" {
+  description = "Tag for Private Subnet"
+  type        = string
+  default     = "new_database_subnet_az_1a"
+}
+variable "database_subnet_tag_2" {
+  description = "Tag for Private Subnet"
+  type        = string
+  default     = "new_database_subnet_az_1b"
+}
+variable "map_public_ip_on_launch" {
+  description = "It will map the public ip while launching resources"
+  type        = bool
+  default     = null
+}
+variable "database_subnets_cidr_1" {
+  description = "mention the CIDR block for database subnet"
+  type = string
+  default = "10.3.0.0/24"
+}
+variable "database_subnets_cidr_2" {
+  description = "mention the CIDR block for database subnet"
+  type = string
+  default = "10.4.0.0/24"
+}
